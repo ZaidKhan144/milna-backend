b70193ada9710652c415a161775c21a00f3cf656
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-05-03 13:25:58 -0500

    backend and mongoDB setup

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "20mb", extended: true}))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000;
+
+// connecting to mongoDB
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, ()=> 
+    console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
+
+
